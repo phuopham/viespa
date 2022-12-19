4be531d8a7f4fae9cb8305cb452f1c55c38d96b7
Nam Hoang Nguyen <nam.nh.2068@aptechlearning.edu.vn>
2022-12-19 11:34:19 +0700

    redundant import cause bug

--- src/main/java/com/spa/viespa/services/StaffService.java
+++ src/main/java/com/spa/viespa/services/StaffService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
